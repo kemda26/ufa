facf036c4e81265e8dfdf7e98f7545d792799523
sugarless <quangnd@foobla.com>
2019-05-28 13:02:52 +0700

    commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,25 +4,25 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
-  root: {
-    padding: '2px 2px',
-  },
+    root: {
+        padding: '2px 2px',
+    },
 }
 
 function PaperSheet() {
 
-  return (
-    <div>
-      <Paper style={styles.root}>
-        <Typography variant="h5" component="h3">
-            Welcome to Ufacilities
-        </Typography>
-        <Typography component="p">
-            UFA
-        </Typography>
-      </Paper>
-    </div>
-  );
+    return (
+        <div>
+            <Paper style={styles.root}>
+                <Typography variant="h5" component="h3">
+                    Welcome to Ufacilities
+                </Typography>
+                <Typography component="p">
+                    UFA
+                </Typography>
+            </Paper>
+        </div>
+    );
 }
 
 export default PaperSheet;
